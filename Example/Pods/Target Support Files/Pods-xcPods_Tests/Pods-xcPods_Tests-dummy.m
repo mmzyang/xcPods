17249414c6de5271719af0891351e6ed7c9d64d2
xuci <xuci@fuse.co.id>
2022-04-07 17:35:02 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-xcPods_Tests/Pods-xcPods_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xcPods_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_xcPods_Tests
+@end
---
+++ Example/Pods/Target Support Files/xcPods/xcPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_xcPods : NSObject
+@end
+@implementation PodsDummy_xcPods
+@end
