9634ed1a854138bb816335462d3cfdea5fba08a4
xuci <xuci@fuse.co.id>
2022-04-07 17:36:21 +0800

    commit

---
+++ xcPods/Classes/XCTest.h
@@ -0,0 +1,18 @@
+//
+//  XCTest.h
+//  GLCProject
+//
+//  Created by 徐慈 on 2022/4/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTest : NSObject
+
++ (void)printMessage;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ xcPods/Classes/XCTest.m
@@ -0,0 +1,17 @@
+//
+//  XCTest.m
+//  GLCProject
+//
+//  Created by 徐慈 on 2022/4/7.
+//
+
+#import "XCTest.h"
+
+@implementation XCTest
+
+
++ (void)printMessage {
+    NSLog(@"我是一条测试打印数据");
+}
+
+@end
